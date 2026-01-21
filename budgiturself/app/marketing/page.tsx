db79f420c15f69105bcff2386b5f2db1efced78f
tyler <tyler@bestie.biz>
2026-01-20 21:03:57 -0600

    Boilerplate for frontend structure

---
+++ budgiturself/app/marketing/page.tsx
@@ -0,0 +1,7 @@
+export default function marketingPage() {
+    return (
+        <div>
+            <h1>Welcome to Marketing Page</h1>
+        </div>
+    )
+}
--- budgiturself/app/page.tsx
+++ budgiturself/app/page.tsx
@@ -1,9 +1,3 @@
-import {redirect} from "next/dist/client/components/redirect";
-
 export default function Home() {
-  redirect("/home");
 
-  return (
-      <h1>Here</h1>
-  )
 }
