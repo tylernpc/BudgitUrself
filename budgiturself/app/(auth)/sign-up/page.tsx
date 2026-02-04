b3c8c4176164464bb01b35a753b57f217887c16b
tyler <tyler@bestie.biz>
2026-02-03 19:22:07 -0600

    Login Section Centered

--- budgiturself/app/(auth)/sign-up/page.tsx
+++ budgiturself/app/(auth)/sign-up/page.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
-import {DollarSign} from "lucide-react";
+import { DollarSign } from "lucide-react";
 
 export default function SignUpPage() {
     return (
-        <section>
+        <section className="min-h-screen flex flex-col">
             <Link href="/">
                 <button className="inline-flex items-center text-3xl pt-6 pl-6 cursor-pointer">
                     <DollarSign className="size-7 text-blue-700"></DollarSign>
                     <span className="text-blue-700">Budgit</span>Urself
                 </button>
             </Link>
+
+            <div className="flex-1 flex items-center justify-center">
+                <div className="flex flex-col items-center">
+                    <h1>Login Section</h1>
+                </div>
+            </div>
         </section>
     );
 }
