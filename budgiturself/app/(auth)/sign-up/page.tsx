d19dc5739de863de043726b350895e121f60316f
tyler <tyler@bestie.biz>
2026-02-03 19:17:10 -0600

    BudgitUrself Logo and Link back to main page onClick

--- budgiturself/app/(auth)/sign-up/page.tsx
+++ budgiturself/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+import {DollarSign} from "lucide-react";
+
 export default function SignUpPage() {
-  return (
-    <main>
-      <h1>Sign Up</h1>
-    </main>
-  );
+    return (
+        <section>
+            <Link href="/">
+                <button className="inline-flex items-center text-3xl pt-6 pl-6 cursor-pointer">
+                    <DollarSign className="size-7 text-blue-700"></DollarSign>
+                    <span className="text-blue-700">Budgit</span>Urself
+                </button>
+            </Link>
+        </section>
+    );
 }
