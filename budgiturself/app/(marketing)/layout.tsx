0e9c3ea578b3dfcb40684369a4f59749c7eff577
tyler <tyler@bestie.biz>
2026-02-03 20:39:36 -0600

    Analytics so we can check visitors

--- budgiturself/app/(marketing)/layout.tsx
+++ budgiturself/app/(marketing)/layout.tsx
@@ -1,10 +1,12 @@
 import type {ReactNode} from "react";
 import {SpeedInsights} from "@vercel/speed-insights/next"
+import {Analytics} from "@vercel/analytics/next"
 
 export default function MarketingLayout({children,}: {
     children: ReactNode;
 }) {
     return <main>{children}
         <SpeedInsights/>
+        <Analytics/>
     </main>;
 }
