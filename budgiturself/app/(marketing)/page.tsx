de06e3a3aea42096012cc7c590befdb88ecd9ab1
tyler <tyler@bestie.biz>
2026-02-19 17:39:08 -0600

    Shows landing page if user isn't authenticated

--- budgiturself/app/(marketing)/page.tsx
+++ budgiturself/app/(marketing)/page.tsx
@@ -3,15 +3,22 @@ import {Features} from "@/components/marketing/features";
 import {InteractiveDemo} from "@/components/marketing/interactive-demo";
 import {Categories} from "@/components/marketing/categories";
 import {Footer} from "@/components/marketing/footer";
+import {auth0} from "@/lib/auth/auth0";
 
-export default function MarketingPage() {
-    return (
-        <main className="min-h-screen bg-white">
-            <Hero/>
-            <Features/>
-            <InteractiveDemo/>
-            <Categories/>
-            <Footer/>
-        </main>
-    );
+export default async function MarketingPage() {
+    const session = await auth0.getSession();
+
+    if (!session) {
+        return (
+            <main className="min-h-screen bg-white">
+                <Hero/>
+                <Features/>
+                <InteractiveDemo/>
+                <Categories/>
+                <Footer/>
+            </main>
+        );
+    }
+
+    // todo: tyler, implement a return here that redirects the user to their homepage
 }
